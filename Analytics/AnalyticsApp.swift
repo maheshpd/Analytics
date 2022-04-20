52715c084a6cbcf2170973eccfe0f69d65d81b32
maheshprasad <mahesh223prasad@gmail.com>
2022-04-20 18:04:12 +0530

    Initial Commit

---
+++ Analytics/AnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsApp.swift
+//  Analytics
+//
+//  Created by Mahesh Prasad on 20/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
